d994d5783ee9b01496bfe491f7902937841bd69b
Petrarca <pedropetrarcar@gmail.com>
2022-09-13 16:55:09 -0300

    update 1

--- products.js
+++ products.js
@@ -88,3 +88,62 @@ const products = [
     imageDescription: "",
   },
 ];
+
+let frutas = document.getElementsByClassName('fruits')
+let drinks = document.getElementsByClassName('drinks')
+let higiene = document.getElementsByClassName('hygiene')
+
+let productMain = document.createElement('main')
+
+let listHigieneId = document.getElementById("higieneNova")
+let listBebidasID = document.getElementById("bebidasNova")
+let listFrutasID = document.getElementById("frutasNova")
+
+  function createCards(elements) {
+    
+  for(let i = 0; i < elements.length; i++) { 
+    let productCard = document.createElement('li')
+    let productImg = document.createElement('img')
+    let productMain = document.createElement('main')
+    let prodcutTitle = document.createElement('h1')
+    let productCategory = document.createElement('h5')
+    let productPriceStrong = document.createElement('strong')
+
+    productCard.classList.add("product")
+    productImg.classList.add("product-img")
+    productMain.classList.add("product-main")
+    prodcutTitle.classList.add("product-title")
+    productCategory.classList.add("product-category")
+    productPriceStrong.classList.add('product-price')
+
+    if(elements[i].image == undefined){
+      productImg.src = "./img/products/no-img.svg"
+    } else {
+      productImg.src = elements[i].image
+    }
+    productImg.alt = elements[i].imageDescription
+
+    prodcutTitle.innerText = elements[i].title
+    productCategory.innerText = elements[i].category
+    productPriceStrong.innerText = elements[i].price
+    
+    
+    console.log(elements[i].category)
+    if(elements[i].category == "Higiene"){
+    listHigieneId.appendChild(productCard)
+    productCard.append(productImg, productMain)
+    productMain.append(prodcutTitle, productCategory, productPriceStrong)
+    } else if(elements[i].category == "Bebidas"){
+    listBebidasID.appendChild(productCard)
+    productCard.append(productImg, productMain)
+    productMain.append(prodcutTitle, productCategory, productPriceStrong)
+    } else if(elements[i].category == "Frutas"){
+      listFrutasID.appendChild(productCard)
+      productCard.append(productImg, productMain)
+      productMain.append(prodcutTitle, productCategory, productPriceStrong)
+    }
+
+  }
+
+}
+createCards(products)
